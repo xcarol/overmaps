b7c5ebb1dc754885a7c8fbc92fde807a72fbcba0
Xavier Carol <xcarolr@gmail.com>
2024-01-09 19:00:58 +0100

    move maps to class attributes

--- lib/home.dart
+++ lib/home.dart
@@ -2,7 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:overmap/map.dart';
 
 class Home extends StatelessWidget {
-  const Home({super.key});
+  Home({super.key}) {
+    stackedMaps.add(backMap);
+    stackedMaps.add(frontMap);
+  }
+
+  final MapLayer frontMap =
+      const MapLayer(latitude: 41.4471787, longitude: 2.1920866, opacity: 0.5);
+  final MapLayer backMap =
+      const MapLayer(latitude: -33.86, longitude: 151.20, opacity: 1.0);
+  final List<Widget> stackedMaps = const <Widget>[];
 
   @override
   Widget build(BuildContext context) {
@@ -10,10 +19,7 @@ class Home extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Overmap'),
       ),
-      body: const Stack(children: <Widget>[
-        MapLayer(latitude: -33.86, longitude: 151.20, opacity: 1.0),
-        MapLayer(latitude: 41.4471787, longitude: 2.1920866, opacity: 0.5)
-      ]),
+      body: Stack(children: stackedMaps)
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class _MyAppState extends State {
         brightness: Brightness.dark,
       ),
       title: 'Overmap',
-      home: const Home(),
+      home: Home(),
     );
   }
 }
