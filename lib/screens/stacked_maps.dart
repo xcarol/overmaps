eff349f4ef70d8c008efc3710055896e6d26d95e
Xavier Carol <xcarolr@gmail.com>
2024-02-21 19:03:13 +0100

    add support for boundaries and change the update map mechanism

--- lib/screens/stacked_maps.dart
+++ lib/screens/stacked_maps.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:overmap/services/places_service.dart';
 import 'package:overmap/widgets/over_map.dart';
 import 'package:overmap/models/stacked_maps_model.dart';
 import 'package:provider/provider.dart';
@@ -17,20 +18,29 @@ class _StackedMapsState extends State<StackedMaps> {
   GoogleMapController? _frontController;
   GoogleMapController? _backController;
 
+  Set<Polyline>? _frontPlacePolyline;
+  Set<Polyline>? _backPlacePolyline;
+
   late CameraPosition _frontCameraPosition =
-      CameraPosition(target: StackedMapsModel.sydneyLocation);
-  late CameraPosition _backCameraPosition =
       CameraPosition(target: StackedMapsModel.barcelonaLocation);
+  late CameraPosition _backCameraPosition =
+      CameraPosition(target: StackedMapsModel.sydneyLocation);
 
-  get backMap => OverMap(
-      latLng: _backCameraPosition.target,
-      onMapCreated: backMapCreated(),
-      onCameraMove: backCameraMove());
-
-  get frontMap => OverMap(
-      latLng: _frontCameraPosition.target,
+  frontMap(StackedMapsModel map) => OverMap(
+      place: map.frontPlaceName,
+      coordinates: _frontCameraPosition.target,
+      boundaries: _frontPlacePolyline ??
+          {Polyline(polylineId: StackedMapsModel.frontPlacePolylineId)},
       onMapCreated: frontMapCreated(),
       onCameraMove: frontCameraMove());
+
+  backMap(StackedMapsModel map) => OverMap(
+      place: map.backPlaceName,
+      coordinates: _backCameraPosition.target,
+      boundaries: _backPlacePolyline ??
+          {Polyline(polylineId: StackedMapsModel.backPlacePolylineId)},
+      onMapCreated: backMapCreated(),
+      onCameraMove: backCameraMove());
 
   double get frontMapOpacity => (_opacity > StackedMapsModel.halfOpacity
       ? _opacity
@@ -77,8 +87,10 @@ class _StackedMapsState extends State<StackedMaps> {
     };
   }
 
-  CameraPosition updateCameraLocation(GoogleMapController? controller,
-      CameraPosition cameraPosition, LatLng location) {
+  CameraPosition updateCameraLocation(
+    CameraPosition cameraPosition,
+    LatLng location,
+  ) {
     return CameraPosition(
         target: location,
         bearing: cameraPosition.bearing,
@@ -97,31 +109,102 @@ class _StackedMapsState extends State<StackedMaps> {
 
     OverMap.setCameraPosition(_frontController, _frontCameraPosition);
     OverMap.setCameraPosition(_backController, _backCameraPosition);
+
+    Set<Polyline>? copyPlacePolyline = _backPlacePolyline;
+    _backPlacePolyline = _frontPlacePolyline;
+    _frontPlacePolyline = copyPlacePolyline;
+  }
+
+  void setFrontMapBoundary(StackedMapsModel map) async {
+    var value = await getMapBoundary(map.frontPlaceName, map.frontPlaceLocation,
+        StackedMapsModel.frontPlacePolylineId);
+    setState(() {
+      _frontPlacePolyline = value;
+    });
+  }
+
+  void setBackMapBoundary(StackedMapsModel map) async {
+    var value = await getMapBoundary(map.backPlaceName, map.backPlaceLocation,
+        StackedMapsModel.backPlacePolylineId);
+    setState(() {
+      _backPlacePolyline = value;
+    });
+  }
+
+  Future<Set<Polyline>> getMapBoundary(
+      String place, LatLng coordinates, PolylineId polylineId) async {
+    PlacesService placesService = PlacesService(
+        googleMapsApiKey: const String.fromEnvironment("MAPS_API_KEY"));
+
+    List<String> polygons = await placesService.getPlaceBoundaryPolygons(
+        place, coordinates.latitude, coordinates.longitude);
+
+    return Future(() => getBoundaries(polygons));
+  }
+
+  Set<Polyline> getBoundaries(List<String> polygons) {
+    Set<Polyline> boundaries = {};
+    
+    for (String polygon in polygons) {
+      List<LatLng> polylinePoints = List.empty(growable: true);
+      List<String> coordinates = polygon.split(' ');
+
+      for (String coordinatePair in coordinates) {
+        List<String> latLng = coordinatePair.split(',');
+        if (latLng.length == 2) {
+          polylinePoints.add(LatLng(double.parse(latLng[1]), double.parse(latLng[0])));
+        }
+      }
+
+      if (polylinePoints.isNotEmpty) {
+        boundaries.add(Polyline(
+            polylineId: PolylineId(DateTime.now().toString()),
+            points: polylinePoints,
+            width: 1,
+            color: Colors.red));
+      }
+    }
+    
+    return boundaries;
   }
 
   @override
   Widget build(BuildContext context) {
     return Consumer<StackedMapsModel>(builder: (context, map, child) {
+      if (_frontPlacePolyline == null) {
+        setFrontMapBoundary(map);
+      }
+      if (_backPlacePolyline == null) {
+        setBackMapBoundary(map);
+      }
+
       if (needSwitchMaps(map)) {
         switchMaps();
       }
 
-      if (map.updateFrontMapLocation) {
-        _frontCameraPosition = updateCameraLocation(
-            _frontController, _frontCameraPosition, map.frontPlaceLocation);
+      if (map.updateFrontMap) {
+        _frontCameraPosition =
+            updateCameraLocation(_frontCameraPosition, map.frontPlaceLocation);
+
         updateMap(_frontController, _frontCameraPosition);
-        map.updateFrontMapLocation = false;
+        setFrontMapBoundary(map);
+
+        map.resetUpdateFrontMap();
       }
 
-      if (map.updateBackMapLocation) {
-        _backCameraPosition = updateCameraLocation(
-            _backController, _backCameraPosition, map.backPlaceLocation);
+      if (map.updateBackMap) {
+        _backCameraPosition =
+            updateCameraLocation(_backCameraPosition, map.backPlaceLocation);
+
         updateMap(_backController, _backCameraPosition);
-        map.updateBackMapLocation = false;
+        setBackMapBoundary(map);
+
+        map.resetUpdateBackMap();
       }
 
       _opacity = map.opacity;
-      return Stack(children: stackedMaps(frontMap, backMap));
+
+      return Stack(children: stackedMaps(frontMap(map), backMap(map)));
     });
   }
 }
