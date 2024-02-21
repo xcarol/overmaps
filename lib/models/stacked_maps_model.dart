e40bb78cce1ca310551bf5f7953335a948476d6e
Xavier Carol <xcarolr@gmail.com>
2024-02-21 18:41:26 +0100

    add support for boundaries and change the update map mechanism

--- lib/models/stacked_maps_model.dart
+++ lib/models/stacked_maps_model.dart
@@ -4,26 +4,47 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 class StackedMapsModel extends ChangeNotifier {
   static LatLng sydneyLocation = const LatLng(-33.8698439, 151.2082848);
   static LatLng barcelonaLocation = const LatLng(41.3828939, 2.1774322);
-  static String barcelonaName = "Barcelona";
-  static String sydneyName = "Sydney";
+  static String barcelonaName = 'Barcelona';
+  static String sydneyName = 'Sydney';
+  static double initialOpacity = 0.3;
   static double halfOpacity = 0.5;
   static double opaque = 1.0;
   static double defaultZoom = 11.0;
+  static PolylineId frontPlacePolylineId =
+      const PolylineId('frontPlacePolylineId');
+  static PolylineId backPlacePolylineId =
+      const PolylineId('backPlacePolylineId');
 
-  bool updateFrontMapLocation = false;
-  bool updateBackMapLocation = false;
+  bool _updateFrontMap = false;
+  bool _updateBackMap = false;
 
-  double _opacity = halfOpacity;
-  LatLng _frontPlaceLocation = sydneyLocation;
-  LatLng _backPlaceLocation = barcelonaLocation;
-  String _frontPlaceName = sydneyName;
-  String _backPlaceName = barcelonaName;
+  double _opacity = initialOpacity;
+  LatLng _frontPlaceLocation = barcelonaLocation;
+  LatLng _backPlaceLocation = sydneyLocation;
+  String _frontPlaceName = barcelonaName;
+  String _backPlaceName = sydneyName;
 
+  Polyline _frontPlacePolyline = Polyline(polylineId: frontPlacePolylineId);
+  Polyline _backPlacePolyline = Polyline(polylineId: backPlacePolylineId);
+
+  bool get updateFrontMap => _updateFrontMap;
+  bool get updateBackMap => _updateBackMap;
   double get opacity => _opacity;
   LatLng get frontPlaceLocation => _frontPlaceLocation;
   LatLng get backPlaceLocation => _backPlaceLocation;
   String get frontPlaceName => _frontPlaceName;
   String get backPlaceName => _backPlaceName;
+
+  Polyline get frontPlacePolyline => _frontPlacePolyline;
+  Polyline get backPlacePolyline => _backPlacePolyline;
+
+  resetUpdateFrontMap() {
+    _updateFrontMap = false;
+  }
+
+  resetUpdateBackMap() {
+    _updateBackMap = false;
+  }
 
   set opacity(double opacity) {
     _opacity = opacity;
@@ -32,11 +53,13 @@ class StackedMapsModel extends ChangeNotifier {
 
   set frontPlaceLocation(LatLng location) {
     _frontPlaceLocation = location;
+    _updateFrontMap = true;
     notifyListeners();
   }
 
   set backPlaceLocation(LatLng location) {
     _backPlaceLocation = location;
+    _updateBackMap = true;
     notifyListeners();
   }
 
@@ -47,6 +70,16 @@ class StackedMapsModel extends ChangeNotifier {
 
   set backPlaceName(String name) {
     _backPlaceName = name;
+    notifyListeners();
+  }
+
+  set frontPlacePolyline(Polyline polyline) {
+    _frontPlacePolyline = polyline;
+    notifyListeners();
+  }
+
+  set backPlacePolyline(Polyline polyline) {
+    _backPlacePolyline = polyline;
     notifyListeners();
   }
 }
