b102aea960515157a1156acc2f08dc7a871be7af
Xavier Carol <xcarolr@gmail.com>
2024-03-02 18:29:40 +0100

    add go router

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,28 @@ import 'package:flutter/material.dart';
 import 'package:overmap/screens/home.dart';
 import 'package:overmap/models/stacked_maps_model.dart';
 import 'package:provider/provider.dart';
+import 'package:go_router/go_router.dart';
 
-void main() => runApp(ChangeNotifierProvider(
-      create: (context) => StackedMapsModel(),
-      child: const MyApp(),
-    ));
+final router = GoRouter(
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (_, __) => ChangeNotifierProvider(
+        create: (context) => StackedMapsModel(),
+        child: const MyApp(),
+      ),
+      routes: [
+        GoRoute(
+          path: 'details',
+          builder: (_, __) => Scaffold(
+            appBar: AppBar(title: const Text('Details Screen')),
+          ),
+        ),
+      ],
+    ),
+  ],
+);
+void main() => runApp(MaterialApp.router(routerConfig: router));
 
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
